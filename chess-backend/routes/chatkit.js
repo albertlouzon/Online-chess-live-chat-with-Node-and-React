65ea56571b43960f1f55dd5691eaa2e5dfbf1704
unknown <albertlouzon@gmail.com>
2018-10-10 02:34:24 +0200

    back to localhost settings

--- chess-backend/routes/chatkit.js
+++
@@ -1,8 +0,0 @@
-var Chatkit = require('@pusher/chatkit-server');
-
-const chatkit = new Chatkit.default({
-    instanceLocator: 'v1:us1:7bc01ee4-56d0-4947-9406-75a8fffd5adc',
-    key: '384ab8d2-7bbc-472b-9f68-03d2eb357635:tkzmjuHAn3Fps87neOj6UUTT3oZmHqUaeeaJDcQR7Gc='
-});
-
-module.exports = chatkit;
